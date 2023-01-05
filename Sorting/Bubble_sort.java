8dbb72528183ea5e7246d3bb9ae906242d3112fc
siddartha-10 <107453857+siddartha-10@users.noreply.github.com>
2023-01-05 16:42:40 -0600

    selection sort initial commit

--- Sorting/Bubble_sort.java
+++ Sorting/Bubble_sort.java
@@ -1,5 +1,27 @@
+import java.util.Arrays;
+
 public class Bubble_sort {
     public static void main(String[] args){
-        
+        int[] arr = {3,1,4,2,5};
+        bubble(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    public static void bubble(int[] arr){
+        // for n-1 runs
+        for(int i=0;i<arr.length;i++){
+            boolean swapped = false;
+            //each step
+            for(int j=1;j<arr.length-i;j++){
+                if(arr[j-1]>arr[j]){
+                    int temp = arr[j-1];
+                    arr[j-1] = arr[j];
+                    arr[j] = temp;
+                    swapped = true;
+                }
+            }
+            if(swapped==false){
+                break;
+            }
+        }
     }
 }
