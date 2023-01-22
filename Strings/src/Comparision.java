f9642a510584991ee37be96de819771bcd32243b
siddartha-10 <107453857+siddartha-10@users.noreply.github.com>
2023-01-22 13:41:39 -0600

    Comparision commit

---
+++ Strings/src/Comparision.java
@@ -0,0 +1,5 @@
+public class Comparision {
+    public static void main(String[] args) {
+
+    }
+}
--- Strings/src/Main.java
+++ Strings/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String a = "Messi";
+        String b = "Messi";
+        System.out.println(a);
+        System.out.println(b);
+        a = "Lionel"; // I am not channging the object i am creating a new varibale.
+        System.out.println(a);
     }
 }
