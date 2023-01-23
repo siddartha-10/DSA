c9533609956fd78208b894aacb453c689a8507b8
siddartha-10 <107453857+siddartha-10@users.noreply.github.com>
2023-01-22 19:23:44 -0600

    intial opreators commit

--- Strings/src/Comparision.java
+++ Strings/src/Comparision.java
@@ -1,5 +1,15 @@
 public class Comparision {
     public static void main(String[] args) {
 
+        String c = "Messi";
+        String d = "Messi";
+        //System.out.println(c==d); // this will return true because both the reference varibales point to the same object
+        String a = new String("Messi");
+        String b = new String("Messi");
+        //System.out.println(a==b); // this will return false as both the reference variables are point to different objects
+        // as we used the new keyword
+
+        // compare only the values and not the reference then we use .equals() method
+        System.out.println(a.equals(b));
     }
 }
---
+++ Strings/src/PrettyPrinting.java
@@ -0,0 +1,8 @@
+public class PrettyPrinting {
+    public static void main(String[] args) {
+        float a = 453.1264f;
+        System.out.printf("Fromatted Text is %.2f%n", a);
+        float b = 5.124f;
+        System.out.printf("Formatted Text is %.2f", b);
+    }
+}
