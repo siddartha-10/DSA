b3828791ba250ebf15aaa6a8e7e126de2373c3d7
siddartha-10 <107453857+siddartha-10@users.noreply.github.com>
2023-01-26 11:29:57 -0600

    question5 commit

--- Recursion/src/Recursion_Basic_Problems_Striver.java
+++ Recursion/src/Recursion_Basic_Problems_Striver.java
@@ -1,17 +1,58 @@
+import java.util.Scanner;
+
 public class Recursion_Basic_Problems_Striver {
-    public static int c = 0; // global varibale assigning.
     public static void main(String[] args) {
-        print();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        //print(1,n);
+        //print1(1,n);
+        //print2(1,n);
+        //print3(n,n);
+        print4(1,n);
     }
 
     // question 1:- print name 5 times
-    public static void print(){
-        if(c==5){
+    public static void print(int i, int n){
+        if(i>n){
             return;
         }
         System.out.println("Messi");
-        c++;
-        print();
+        i++;
+        print(n,1);
     }
 
+    // question 2:- print linearly from 1 to n
+    public static void print1(int i, int n){
+        if(i>n){
+            return;
+        }
+        System.out.println(i);
+        i++;
+        print1(i,n);
+    }
+    // question 3:- print linearly from n to 1
+    public static void print2(int i, int n){
+        if(n<i){
+            return;
+        }
+        System.out.println(n);
+        n--;
+        print2(i,n);
+    }
+    // question 4:- print linearly from 1 to n (by backtracking)
+    public static void print3(int i, int n){
+        if(i<1){
+            return;
+        }
+        print3(i-1,n);
+        System.out.println(i);
+    }
+    // question 5:- print linearly from n to 1 (by backtracking)
+    public static void print4(int i, int n){
+        if(i>n){
+            return;
+        }
+        print4(i+1,n);
+        System.out.println(i);
+    }
 }
