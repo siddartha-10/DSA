c6754992c564ee851538167fce7ccffb06b58b18
siddartha-10 <107453857+siddartha-10@users.noreply.github.com>
2023-01-27 13:21:46 -0600

    commit

--- Recursion/src/permutations_2_striver_leetcode_47.java
+++ Recursion/src/permutations_2_striver_leetcode_47.java
@@ -1,3 +1,44 @@
+// https://leetcode.com/problems/permutations-ii/description/
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class permutations_2_striver_leetcode_47 {
-    
+    public static void main(String[] args) {
+        int[] arr= {1,1,2};
+        System.out.println(permuteUnique(arr));
+
+    }
+
+    public static List<List<Integer>> permuteUnique(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
+        perm(nums,0,ans);
+        return ans;
+    }
+
+    public static void perm(int[] arr,int index,List<List<Integer>> ans){
+        if(index==arr.length){
+            ArrayList<Integer> ds = new ArrayList<>();
+            for(int i=0;i<arr.length;i++){
+                ds.add(arr[i]);
+            }
+            ans.add(new ArrayList<>(ds));
+            return;
+        }
+        for(int i=index;i<arr.length;i++){
+            if(i!=index && arr[i]==arr[i-1]) continue;
+            swap(i,index,arr);
+            perm(arr,index+1,ans);
+            swap(i,index,arr);
+        }
+    }
+
+    public static void swap(int i,int index,int[] arr){
+        int temp = arr[i];
+        arr[i] = arr[index];
+        arr[index] = temp;
+    }
+
+
 }
