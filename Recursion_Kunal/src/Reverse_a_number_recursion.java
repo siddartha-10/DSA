9b5de015dc54380026666c0dd16c08e5a28c43ba
siddartha-10 <107453857+siddartha-10@users.noreply.github.com>
2023-01-28 12:20:19 -0600

    commit

--- Recursion_Kunal/src/Reverse_a_number_recursion.java
+++ Recursion_Kunal/src/Reverse_a_number_recursion.java
@@ -1,10 +1,13 @@
 public class Reverse_a_number_recursion {
     public static void main(String[] args) {
         int n = 1824;
-        rev(n);
-        System.out.println(sum);
+        //rev(n);
+        //System.out.println(sum);
+        System.out.println(rev2(n));
+
     }
-// way 1 take the number outside.
+/*
+ way 1 take the number outside.
     static int sum = 0;
     public static void rev(int n){
         if(n==0){
@@ -13,6 +16,22 @@ public class Reverse_a_number_recursion {
         int rem = n%10;
         sum = sum * 10 + rem;
         rev(n/10);
+    }
+ way 2
+*/
+
+    public static int rev2(int n){
+        int digits = (int)(Math.log10(n)) + 1;
+        System.out.println(digits);
+        return helper(n,digits);
+    }
+
+    public static int helper(int n,int digits){
+        if(n%10==n){
+            return n;
+        }
+        int rem = n%10;
+        return rem * (int)(Math.pow(10,digits-1)) + helper(n/10,digits-1);
     }
 
 }
