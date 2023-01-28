0eb3367b000c39e6ae18cac5bc2a588744a9c4a6
siddartha-10 <107453857+siddartha-10@users.noreply.github.com>
2023-01-28 14:00:21 -0600

    returning array_list done

--- Recursion_Kunal/src/Linear_search_recursion.java
+++ Recursion_Kunal/src/Linear_search_recursion.java
@@ -1,16 +1,46 @@
+import java.util.ArrayList;
+
 public class Linear_search_recursion {
     public static void main(String[] args) {
         int[] arr = {2,4,99,2,1};
         int target = 99;
-        System.out.println(linear_search(arr,target,0));
+//        ls(arr,target,0);
+//        System.out.println(ans);
+        ArrayList<Integer> b = new ArrayList<>();
+        System.out.println(ls1(arr,target,0,b));
     }
 
     public static boolean linear_search(int[] arr,int target,int index){
-        if(index==arr.length-1){
+        if(index==arr.length){
             return false;
         }
         if(arr[index]==target){return true;}
         return linear_search(arr,target,index+1);
+    }
+
+    // when we find a target then add it to the array list
+
+    static ArrayList<Integer> ans = new ArrayList<>();
+    public static void ls(int[] arr,int target,int index){
+        if(index==arr.length){
+            return;
+        }
+        if(arr[index]==target){
+            ans.add(index);
+        }
+        ls(arr,target,index+1);
+    }
+
+    // returning an array list that contains the index of the targets.
+
+    public static ArrayList<Integer> ls1(int[] arr,int target,int index,ArrayList<Integer> b){
+        if(index==arr.length){
+            return b;
+        }
+        if(arr[index]==target){
+            b.add(index);
+        }
+        return ls1(arr,target,index+1,b);
     }
 
 }
