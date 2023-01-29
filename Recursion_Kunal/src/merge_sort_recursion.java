cd8e492341d62ec0baf38cefe21c9366c7d3d833
siddartha-10 <107453857+siddartha-10@users.noreply.github.com>
2023-01-28 21:28:28 -0600

    merge_sort_INplace

--- Recursion_Kunal/src/merge_sort_recursion.java
+++ Recursion_Kunal/src/merge_sort_recursion.java
@@ -5,10 +5,13 @@ import java.util.Arrays;
 public class merge_sort_recursion {
     public static void main(String[] args) {
         int[] arr = {8,3,4,6,5,12};
-        arr = mergeSort(arr);
+//        arr = mergeSort(arr);
+//        System.out.println(Arrays.toString(arr));
+        mergeSortInplace(arr,0,arr.length);
         System.out.println(Arrays.toString(arr));
     }
 
+/*
     public static int[] mergeSort(int[] arr){
         if(arr.length==1){
             return arr;
@@ -20,7 +23,6 @@ public class merge_sort_recursion {
 
         return merge(right,left);
     }
-
     private static int[] merge(int[] first, int[] second) {
         int[] mix = new int[first.length + second.length];
         int i=0;
@@ -51,6 +53,55 @@ public class merge_sort_recursion {
             k++;
         }
         return mix;
+    }
+*/
+
+
+    // merge sort in place
+    public static void mergeSortInplace(int[] arr,int s,int e){
+        if(e-s==1){
+            return;
+        }
+        int mid = s + (e-s) / 2;
+        mergeSortInplace(arr,s,mid);
+        mergeSortInplace(arr,mid,e);
+
+        mergeInplace(arr,s,mid,e);
+    }
+
+    private static void mergeInplace(int[] arr, int s, int mid, int e) {
+        int[] mix = new int[e-s];
+        int i=s;
+        int j=mid;
+        int k=0;
+
+        while(i<mid && j<e){
+            if(arr[i]<arr[j]){
+                mix[k] = arr[i];
+                i++;
+            }
+            else{
+                mix[k] = arr[j];
+                j++;
+            }
+            k++;
+        }
+
+        while(i<mid){
+            mix[k] = arr[i];
+            i++;
+            k++;
+        }
+
+        while(j<e){
+            mix[k] = arr[j];
+            j++;
+            k++;
+        }
+
+        for(int l=0;l<mix.length;l++){
+            arr[s+l] = mix[l];
+        }
     }
 
 }
