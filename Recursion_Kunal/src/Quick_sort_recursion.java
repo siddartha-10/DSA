e51e2b5850e486d463000c826bde759d2f5d17d9
siddartha-10 <107453857+siddartha-10@users.noreply.github.com>
2023-01-31 11:57:41 -0600

    commit

--- Recursion_Kunal/src/Quick_sort_recursion.java
+++ Recursion_Kunal/src/Quick_sort_recursion.java
@@ -1,8 +1,41 @@
+import java.util.Arrays;
+
 public class Quick_sort_recursion {
     public static void main(String[] args) {
-        
+        int[] arr = {5,4,3,1,2};
+        quick_sort(arr,0,arr.length-1);
+        System.out.println(Arrays.toString(arr));
     }
 
+    public static void quick_sort(int[] arr, int low, int high){ // low and high are there to see what part of the array we are working on
+        if(low>=high){
+            return;
+        }
+        int start = low; // the start and the mid are used to swapping the number.
+        int end = high;
+        int mid = start + (end-start) / 2; // this is used to take the pivot element.
 
+        int pivot = arr[mid];
+        while(start<=end){
+            while(arr[start] < pivot){
+                start++;
+            }
+            while (arr[end] > pivot){
+                end--;
+            }
+
+            if(start<=end){
+                int temp  = arr[start];
+                arr[start] = arr[end];
+                arr[end] = temp;
+                start++;
+                end--;
+            }
+        }
+        // now pivot is in the correct index hence what we do is to make the array into 2 parts.
+        quick_sort(arr,low,end);
+        quick_sort(arr,start,high);
+
+    }
 
 }
