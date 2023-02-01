e232e7d1bc275cecadb967a5d1e34811bfa77bc2
siddartha-10 <107453857+siddartha-10@users.noreply.github.com>
2023-02-01 09:27:31 -0600

    returning a string commit

--- Recursion_Kunal/src/skip_a_character_kunal.java
+++ Recursion_Kunal/src/skip_a_character_kunal.java
@@ -1,6 +1,7 @@
 public class skip_a_character_kunal {
     public static void main(String[] args) {
-        skip("","baccdah");
+        skip("","baccdah"); // this is just printing
+        System.out.println(skip1("","baccdah")); // this is returning a string
     }
 
     public static void skip(String p, String up){
@@ -15,6 +16,19 @@ public class skip_a_character_kunal {
         }
         else{
             skip(p+ch,up.substring(1));
+        }
+    }
+
+    public static String skip1(String p,String up){
+        if(up.isEmpty()){
+            return p;
+        }
+        char ch = up.charAt(0);
+        if(ch=='a'){
+            return skip1(p,up.substring(1));
+        }
+        else{
+            return (skip1(p+ch,up.substring(1)));
         }
     }
 
