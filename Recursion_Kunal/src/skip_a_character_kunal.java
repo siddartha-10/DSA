58019871686c99ad69e222ce2adc841a0cc759dc
siddartha-10 <107453857+siddartha-10@users.noreply.github.com>
2023-02-01 09:30:07 -0600

    changed commit

--- Recursion_Kunal/src/skip_a_character_kunal.java
+++ Recursion_Kunal/src/skip_a_character_kunal.java
@@ -1,7 +1,7 @@
 public class skip_a_character_kunal {
     public static void main(String[] args) {
         skip("","baccdah"); // this is just printing
-        System.out.println(skip1("","baccdah")); // this is returning a string
+        System.out.println(skip1("baccdah")); // this is returning a string
     }
 
     public static void skip(String p, String up){
@@ -19,16 +19,16 @@ public class skip_a_character_kunal {
         }
     }
 
-    public static String skip1(String p,String up){
+    public static String skip1(String up){
         if(up.isEmpty()){
-            return p;
+            return "";
         }
         char ch = up.charAt(0);
         if(ch=='a'){
-            return skip1(p,up.substring(1));
+            return skip1(up.substring(1));
         }
         else{
-            return (skip1(p+ch,up.substring(1)));
+            return ch + skip1(up.substring(1));
         }
     }
 
