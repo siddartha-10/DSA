93e31508e2ef15293dd4c606929bcc8bb826c06e
siddartha-10 <107453857+siddartha-10@users.noreply.github.com>
2023-02-01 17:13:36 -0600

    arraylist return added commit

--- Recursion_Kunal/src/String_Subseq_kunal.java
+++ Recursion_Kunal/src/String_Subseq_kunal.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
+
 public class String_Subseq_kunal {
     public static void main(String[] args) {
-        subseq("","abc");
+        //subseq("","abc");
+        System.out.println(subseqarr("","abc"));
     }
 
     public static void subseq(String p,String up){
@@ -11,6 +14,20 @@ public class String_Subseq_kunal {
         char ch = up.charAt(0);
         subseq(p+ch,up.substring(1));
         subseq(p,up.substring(1));
+    }
+
+    public static ArrayList<String> subseqarr(String p, String up){ // returning array list without passsing in parameters.
+        if(up.isEmpty()){
+            ArrayList<String> a = new ArrayList<>();
+            a.add(p);
+            return a;
+        }
+        char ch = up.charAt(0);
+        ArrayList<String> left =  subseqarr(p+ch,up.substring(1));
+        ArrayList<String> right =  subseqarr(p,up.substring(1));
+
+        right.addAll(left);
+        return right;
     }
 
 }
