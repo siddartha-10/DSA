47a5a617a27e7f3e90ae147eeecfc52c051d2bba
siddartha-10 <107453857+siddartha-10@users.noreply.github.com>
2023-02-01 17:53:25 -0600

    commit

---
+++ Recursion_Kunal/src/subset_number_iteration_kunal.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class subset_number_iteration_kunal {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3};
+        System.out.println(subset(arr));
+    }
+
+    public static List<List<Integer>> subset(int[] arr){
+        List<List<Integer>> outer = new ArrayList<>();
+        outer.add(new ArrayList<>());
+        for(int num:arr){
+            int n = outer.size();
+            for (int i = 0; i < n; i++) {
+                ArrayList<Integer> internal = new ArrayList<>(outer.get(i));
+                internal.add(num);
+                outer.add(internal);
+            }
+        }
+        return outer;
+    }
+}
