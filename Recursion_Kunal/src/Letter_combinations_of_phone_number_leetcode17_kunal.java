2ace162491b435b50e4388d2ea8e91ecb8ad6640
siddartha-10 <107453857+siddartha-10@users.noreply.github.com>
2023-02-02 10:34:19 -0600

    final commit

--- Recursion_Kunal/src/Letter_combinations_of_phone_number_leetcode17_kunal.java
+++ Recursion_Kunal/src/Letter_combinations_of_phone_number_leetcode17_kunal.java
@@ -2,34 +2,59 @@ import java.util.ArrayList;
 
 public class Letter_combinations_of_phone_number_leetcode17_kunal {
     public static void main(String[] args) {
-        System.out.println(combi("","12"));
+        String digits = "23";
+//        String[] arr = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+//        combi(arr,"",digits);
+        System.out.println(combinations(digits));
     }
 
-    public static void combinations(String p,String up){
-        if(up.isEmpty()){
-            System.out.println(p);
-            return;
-        }
-        int digit = up.charAt(0) - '0';
-        for (int i = (digit-1)*3; i < digit*3; i++) {
-            char ch = (char)('a'+i);
-            combinations(p+ch,up.substring(1));
-        }
+    public static ArrayList<String> combinations(String digits){
+        String[] arr = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        ArrayList<String> list = new ArrayList<>();
+        list.addAll(combi(arr,"",digits));
+        return list;
     }
-
-    public static ArrayList<String> combi(String p ,String up){
+    public static ArrayList<String> combi(String[] letters,String p,String up){
         if(up.isEmpty()){
             ArrayList<String> list = new ArrayList<>();
             list.add(p);
             return list;
         }
-        int digit = up.charAt(0) - '0';
         ArrayList<String> list = new ArrayList<>();
-        for (int i = (digit-1)*3; i < digit*3; i++) {
-            char ch = (char)('a'+i);
-            list.addAll(combi(p+ch,up.substring(1)));
+        int digit = up.charAt(0) - '0';
+        String a = letters[digit];
+        for (int i = 0; i < a.length(); i++) {
+            list.addAll(combi(letters,p+a.charAt(i),up.substring(1)));
         }
         return list;
     }
 
 }
+
+//    public static void combinations(String p,String up){
+//        if(up.isEmpty()){
+//            System.out.println(p);
+//            return;
+//        }
+//        int digit = up.charAt(0) - '0';
+//        for (int i = (digit-1)*3; i < digit*3; i++) {
+//            char ch = (char)('a'+i);
+//            combinations(p+ch,up.substring(1));
+//        }
+//    }
+//
+//    public static ArrayList<String> combi(String p ,String up){
+//        if(up.isEmpty()){
+//            ArrayList<String> list = new ArrayList<>();
+//            list.add(p);
+//            return list;
+//        }
+//        int digit = up.charAt(0) - '0';
+//        ArrayList<String> list = new ArrayList<>();
+//        for (int i = (digit-1)*3; i < digit*3; i++) {
+//            char ch = (char)('a'+i);
+//            list.addAll(combi(p+ch,up.substring(1)));
+//        }
+//        return list;
+//    }
+
