0166693c7229154570d6e65a44ad31cff25b460e
siddartha-10 <107453857+siddartha-10@users.noreply.github.com>
2023-02-02 16:26:10 -0600

    diagonal commit

--- Recursion_Kunal/src/maze_problem_kunal.java
+++ Recursion_Kunal/src/maze_problem_kunal.java
@@ -1,7 +1,8 @@
 public class maze_problem_kunal {
     public static void main(String[] args) {
         //System.out.println(count(3,3));
-        path("",3,3);
+        //path("",3,3);
+        path_with_diagonals("",3,3);
     }
 
     // count the number of ways to reach the end
@@ -26,6 +27,23 @@ public class maze_problem_kunal {
         if(c>1){
             path(p+"R",r,c-1);
         }
+    }
+
+    public static void path_with_diagonals(String p,int r,int c){
+        if(r==1 && c==1){
+            System.out.println(p);
+            return;
+        }
+        if(r>1 && c>1){ // D is for diagonal
+            path_with_diagonals(p+"D",r-1,c-1);
+        }
+        if(r>1){ // V is for vertical or going downwards
+            path_with_diagonals(p+"V",r-1,c);
+        }
+        if(c>1){ // H is for horizontal
+            path_with_diagonals(p+"H",r,c-1);
+        }
+
     }
 
 }
