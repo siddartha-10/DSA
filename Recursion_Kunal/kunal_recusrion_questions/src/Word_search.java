e2c531d1a33307d232dd8fff881bbe9835a4b442
siddartha-10 <107453857+siddartha-10@users.noreply.github.com>
2023-02-08 17:41:36 -0600

    word_search final

--- Recursion_Kunal/kunal_recusrion_questions/src/Word_search.java
+++ Recursion_Kunal/kunal_recusrion_questions/src/Word_search.java
@@ -1,25 +1,17 @@
+// https://leetcode.com/problems/word-search/description/
 import java.util.Arrays;
-
 public class Word_search {
     public static void main(String[] args) {
         char[][] board = {{'A','B','C','E'},{'S','F','C','S'},{'A','D','E','E'}};
 //        char[][] board = {{'a', 'b'}, {'c', 'd'}};
-        int m = board.length;
-        int n = board[0].length;
-        boolean[][] board1 = new boolean[m][n];
-        String word = "AC";
-        StringBuilder sb = new StringBuilder();
-        System.out.println(find(board, board1, word, sb, 0));
+        String word = "ABCB";
+        System.out.println(find(board, word, 0));
     }
 
-    public static boolean find(char[][] board,boolean[][] board1,String word,StringBuilder sb,int index){
-        if(index==word.length()){
-            return true;
-        }
-        char ch = word.charAt(0);
+    public static boolean find(char[][] board,String word,int index){
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
-                if(isSafe(board1,i,j,ch,board) && board[i][j]==ch){
+                if(isSafe(i,j,board,word,index)){
                     return true;
                 }
             }
@@ -27,36 +19,22 @@ public class Word_search {
         return false;
     }
 
-    public static boolean isSafe(boolean[][] board1,int r,int c,char ch,char[][] board){
-        if(isValid(board1,r,c-1,ch,board)){
-            if(board1[r][c-1]){
+    public static boolean isSafe(int r,int c,char[][] board,String word,int index){
+        if(r>=0 && r<=board.length-1 && c>=0 && c<=board[0].length-1){
+            if(board[r][c]!=word.charAt(index)){
                 return false;
             }
-        }
-        else if(isValid(board1,r,c+1,ch,board)){
-            if(board1[r][c+1]){
-                return false;
+            if(index==word.length()-1){
+                return true;
             }
+            char a = board[r][c];
+            board[r][c] = 'T';
+            boolean result = isSafe(r,c-1,board,word,index+1) || isSafe(r,c+1,board,word,index+1) ||
+                    isSafe(r+1,c,board,word,index+1) || isSafe(r-1,c,board,word,index+1);
+            board[r][c] = a;
+            return result;
         }
-        else if(isValid(board1,r+1,c,ch,board)){
-            if(board1[r+1][c]){
-                return false;
-            }
-        }
-        else if(isValid(board1,r-1,c,ch,board)){
-            if(board1[r-1][c]){
-                return false;
-            }
-        }
-        return true;
-    }
-    public static boolean isValid(boolean[][] board1, int r,int c,char ch,char[][] board){
-        if(r>=0 && r<=board.length-1 && c>=0 && c<=board[0].length-1 && board[r][c]==ch){
-            return true;
-        }
-        else{
-            return false;
-        }
+        return false;
     }
 
 }
