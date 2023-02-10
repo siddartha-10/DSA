8cb0a3a9f69249c746f6a19fdf616780584fd2b2
siddartha-10 <107453857+siddartha-10@users.noreply.github.com>
2023-02-10 17:06:03 -0600

    Create FindUniqueUsingXOR.java

---
+++ Math_DSA/src/FindUniqueUsingXOR.java
@@ -0,0 +1,11 @@
+public class FindUniqueUsingXOR {
+    public static void main(String[] args) {
+        int[] arr = {2,3,4,1,2,1,3,6,4};
+        System.out.println(unique(arr));
+    }
+
+    public static int unique(int[] arr){
+        return ;
+    }
+
+}
