c0c21c158abd096337858a13c5439226df76f862
siddartha-10 <107453857+siddartha-10@users.noreply.github.com>
2023-02-10 15:24:09 -0600

    commit

--- examples/src/Main.java
+++ examples/src/Main.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Main {
+
     public static void main(String[] args) {
         int[] arr = {1,2,3};
         System.out.println(perm(arr));
