8bfde0a8839207959da86b20e65d286b7e5cd649
siddartha-10 <107453857+siddartha-10@users.noreply.github.com>
2023-02-11 11:35:54 -0600

    number of set bits

--- Math_DSA/src/Find_number_of_set_bits.java
+++ Math_DSA/src/Find_number_of_set_bits.java
@@ -1,8 +1,10 @@
 public class Find_number_of_set_bits {
     public static void main(String[] args) {
         System.out.println(length(1));
+        System.out.println(setBits(45));
     }
 
+    // this is one way to get the number of set bits
     public static int length(int num){
         int len = 0;
         while (num > 0){
@@ -12,6 +14,17 @@ public class Find_number_of_set_bits {
             num = num >> 1;
         }
         return len;
+    }
+
+    // way 2 to get the number of set bits
+
+    public static int setBits(int n){
+        int count = 0;
+        while(n>0){
+            count++;
+            n = n - (n & -n);
+        }
+        return count;
     }
 
 }
