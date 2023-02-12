ae2db949b0719745ec11db9377c8084b4b81620e
siddartha-10 <107453857+siddartha-10@users.noreply.github.com>
2023-02-12 11:55:33 -0600

    gcd_lcm commit

---
+++ Math_DSA/src/Gcd_Lcm.java
@@ -0,0 +1,19 @@
+public class Gcd_Lcm {
+    public static void main(String[] args) {
+        System.out.println(gcd(4,8));
+        System.out.println(lcm(9,18));
+    }
+
+    public static int gcd(int a, int b) {
+        if(a==0){
+            return b;
+        }
+        return gcd(b%a,a);
+    }
+
+    public static int lcm(int a, int b) {
+        return a*b / gcd(a,b);
+    }
+
+
+}
