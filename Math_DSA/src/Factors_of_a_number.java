ae8629d7ab3a0dcb7720c05b879e8f225db39b5d
siddartha-10 <107453857+siddartha-10@users.noreply.github.com>
2023-02-12 11:53:24 -0600

    factors commit

--- Math_DSA/src/Factors_of_a_number.java
+++ Math_DSA/src/Factors_of_a_number.java
@@ -1,12 +1,24 @@
 public class Factors_of_a_number {
     public static void main(String[] args) {
         factors(20);
+        System.out.println();
+        factors1(20);
     }
 
+    // way-1 brute force approach
     public static void factors(int n){
         for(int i=1;i<=n;i++){
             if(n%i==0){
                 System.out.print(i+" ");
+            }
+        }
+    }
+
+    // way-2 optimised solution time complexity is O(sqrt(n))
+    public static void factors1(int n){
+        for(int i=1;i<=(int)Math.sqrt(n);i++){
+            if(n%i==0){
+                System.out.print(i+" "+n/i+" ");
             }
         }
     }
