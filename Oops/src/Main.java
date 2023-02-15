a7e368270bc3acbe0f8f8fe101c2ae7d5259dc61
siddartha-10 <107453857+siddartha-10@users.noreply.github.com>
2023-02-15 16:52:42 -0600

    inital commit

--- Oops/src/Main.java
+++ Oops/src/Main.java
@@ -4,12 +4,26 @@ public class Main {
         int[] numbers = new int[5];
         // store 5 names
         String[] names = new String[5];
-    }
 
-    // template of a class or an example of how a class is created
-    class student{
-        int[] numbers = new int[5];
-        String[] names = new String[5];
-        float[] marks = new float[5];
+
+        // this is how we create the objects and instantiate it
+        student student1 = new student();
+        // now the when we print the value of studen1 we the addrees of where the objects has been stored
+
+        // to initialize some values to the variable's we use dot product
+
+        student1.numbers = 10;
+        student1.marks = 96.87f;
+        student1.names = "Messi";
+
+        System.out.println(student1.numbers + "\n"+ student1.marks+ "\n"+ student1.names);
     }
+}
+
+// template of a class or an example of how a class is created
+// this is used for every single student
+class student{
+    int numbers;
+    String names;
+    float marks;
 }
