24d55c21c0492c5720e30e9e8cad75272628fbc1
siddartha-10 <107453857+siddartha-10@users.noreply.github.com>
2023-02-15 15:49:54 -0600

    Oops intial commit

---
+++ Oops/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        // store 5 roll numbers
+        int[] numbers = new int[5];
+        // store 5 names
+        String[] names = new String[5];
+    }
+
+    // template of a class or an example of how a class is created
+    class student{
+        int[] numbers = new int[5];
+        String[] names = new String[5];
+        float[] marks = new float[5];
+    }
+}
