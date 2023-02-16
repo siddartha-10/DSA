4283bda2ee18bd809d1f3dee8f94eee5b259d72f
siddartha-10 <107453857+siddartha-10@users.noreply.github.com>
2023-02-16 15:19:50 -0600

    static blokc
    here i am creating a static block where i can acutally initialise the varibales

--- Oops/src/staticExample/StaticInitialize.java
+++ Oops/src/staticExample/StaticInitialize.java
@@ -1,5 +1,16 @@
 package staticExample;
 
 public class StaticInitialize {
-    
+    static int a = 4;
+    static int b;
+
+    static {
+        System.out.println("I am inside the static block to initialise the variables");
+        b = a*5;
+    }
+
+    public static void main(String[] args) {
+        StaticInitialize obj  = new StaticInitialize();
+        System.out.println(obj.a+ "    "+obj.b);
+    }
 }
