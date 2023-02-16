ce52ff2da4b01887f5831e0e41c9abef9352b738
siddartha-10 <107453857+siddartha-10@users.noreply.github.com>
2023-02-16 10:43:04 -0600

    added packages

---
+++ Oops/src/packages/a/Greetings.java
@@ -0,0 +1,10 @@
+package packages.a;
+
+import static packages.b.Message.message;
+
+public class Greetings {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        message();
+    }
+}
