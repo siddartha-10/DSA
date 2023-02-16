30d01879aed6fbb1877f0496a0252b8ea6ba2a11
siddartha-10 <107453857+siddartha-10@users.noreply.github.com>
2023-02-16 15:24:29 -0600

    statick blokc
    we can only enter only one time that is when the first time the object is created

--- Oops/src/staticExample/StaticInitialize.java
+++ Oops/src/staticExample/StaticInitialize.java
@@ -4,6 +4,8 @@ public class StaticInitialize {
     static int a = 4;
     static int b;
 
+    // the static block will only for one time when the object is created and that's it even if we
+    // create new objects it will not run again and again only one time.
     static {
         System.out.println("I am inside the static block to initialise the variables");
         b = a*5;
@@ -11,6 +13,14 @@ public class StaticInitialize {
 
     public static void main(String[] args) {
         StaticInitialize obj  = new StaticInitialize();
-        System.out.println(obj.a+ "    "+obj.b);
+        System.out.println(StaticInitialize.a+ "    "+StaticInitialize.b);
+
+        obj.b+=3;
+
+
+        StaticInitialize obj1 = new StaticInitialize(); // now when the new object is created we can ionly see one entry
+        // into the static block
+        System.out.println(StaticInitialize.a+ "    "+StaticInitialize.b);
+
     }
 }
