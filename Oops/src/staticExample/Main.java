8ec6b52e1bf827b05ca7a8bbbb8cd3ccd219e272
siddartha-10 <107453857+siddartha-10@users.noreply.github.com>
2023-02-16 15:04:11 -0600

    commit

--- Oops/src/staticExample/Main.java
+++ Oops/src/staticExample/Main.java
@@ -4,11 +4,14 @@ public class Main {
     public static void main(String[] args) {
         Human messi = new Human(35,"Lionel",100000,true);
         Human sid = new Human(22,"sid",1000,false);
+        Human cris = new Human(38,"cris",1000,true);
 
         System.out.println(messi.age+"\t"+messi.name+"\t"+messi.salary+"\t"+messi.marriage);
         System.out.println(sid.age+"\t"+sid.name+"\t"+sid.salary+"\t"+sid.marriage);
-
-        System.out.println(messi.population);
-        System.out.println(sid.population);
+        System.out.println(cris.age+"\t"+cris.name+"\t"+cris.salary+"\t"+cris.marriage);
+        
+        System.out.println(Human.population);
+        System.out.println(Human.population);
+        System.out.println(Human.population);
     }
 }
