3f93c61eaa2605fa76dbb64e8049d9d8f1c90f18
siddartha-10 <107453857+siddartha-10@users.noreply.github.com>
2023-02-18 10:33:56 -0600

    commit

--- Oops/src/Properties/Polymorphism/Main.java
+++ Oops/src/Properties/Polymorphism/Main.java
@@ -5,9 +5,11 @@ public class Main {
         Shapes shape = new Shapes();
         Circle cricle = new Circle();
         Square square = new Square();
+        Shapes s1 = new Square();
 
         shape.area(); // this prints the area method in the Shapes classs because it has it.
         cricle.area(); // this prints the area method in the cricle class as they have it.
         square.area(); // this prints the area method in the Square class as it has it.
+        s1.area(); // this is giving the area of the shape 
     }
 }
