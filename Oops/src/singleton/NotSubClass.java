694da947704e511c8ff3685cc5078a6440098820
siddartha-10 <107453857+siddartha-10@users.noreply.github.com>
2023-02-18 13:27:10 -0600

    commit

--- Oops/src/singleton/NotSubClass.java
+++ Oops/src/singleton/NotSubClass.java
@@ -3,6 +3,6 @@ import access.A;
 public class NotSubClass {
     public static void main(String[] args) {
         NotSubClass obj = new NotSubClass();
-        int n = obj.num; // we are not able to access the protected varibale from not a subclass in different package
+        //int n = obj.num; // we are not able to access the protected varibale from not a subclass in different package
     }
 }
