64dc747eb661ac948c040800f527ecedb945b3ad
siddartha-10 <107453857+siddartha-10@users.noreply.github.com>
2023-02-19 12:53:28 -0600

    object class

--- Oops/src/access/ObjectDemo.java
+++ Oops/src/access/ObjectDemo.java
@@ -2,9 +2,11 @@ package access;
 
 public class ObjectDemo {
     int num;
+    float num2;
 
-    public ObjectDemo(int num) {
+    public ObjectDemo(int num,float num2) {
         this.num = num;
+        this.num2 = num2;
     }
 
     // we have already seen the finalize and tostring method
@@ -21,7 +23,8 @@ public class ObjectDemo {
 
     @Override
     public boolean equals(Object obj) {
-        return super.equals(obj);
+
+        return this.num ==((ObjectDemo)obj).num;
     }
 
     @Override
@@ -32,15 +35,28 @@ public class ObjectDemo {
     }
 
     public static void main(String[] args) {
-        ObjectDemo obj1 = new ObjectDemo(34);
-        ObjectDemo obj2 = new ObjectDemo(34);
-        System.out.println(obj1.hashCode());
-        System.out.println(obj2.hashCode());
-        // the above 2 print statemnets print 2 different hashCode values as the objects of both of them are different
-        // but the below 2 prints the same hashCode values because they are pointing to the same objects
-        ObjectDemo obj3 = new ObjectDemo(34);
-        ObjectDemo obj = obj3;
-        System.out.println(obj3.hashCode());
-        System.out.println(obj.hashCode());
+//        ObjectDemo obj1 = new ObjectDemo(34);
+//        ObjectDemo obj2 = new ObjectDemo(34);
+//        System.out.println(obj1.hashCode());
+//        System.out.println(obj2.hashCode());
+//        // the above 2 print statemnets print 2 different hashCode values as the objects of both of them are different
+//        // but the below 2 prints the same hashCode values because they are pointing to the same objects
+//        ObjectDemo obj3 = new ObjectDemo(34);
+//        ObjectDemo obj = obj3;
+//        System.out.println(obj3.hashCode());
+//        System.out.println(obj.hashCode());
+
+        ObjectDemo obja = new ObjectDemo(12,75.5f);
+        ObjectDemo objb = new ObjectDemo(12,78.9f);
+
+        if(obja == objb){
+            System.out.println("obja is equal to objb");
+        }
+
+        if(obja.equals(objb)){
+            System.out.println("obja is equal to objb");
+        }
+
+        System.out.println(obja.getClass());
     }
 }
