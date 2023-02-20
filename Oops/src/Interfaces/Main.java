9b6291ffcb8b874890327c5b7e9fa8ae39c82cba
siddartha-10 <107453857+siddartha-10@users.noreply.github.com>
2023-02-20 16:05:58 -0600

    commit

--- Oops/src/Interfaces/Main.java
+++ Oops/src/Interfaces/Main.java
@@ -2,9 +2,14 @@ package Interfaces;
 
 public class Main {
     public static void main(String[] args) {
-        Car car = new Car();
+        Engine car = new Car();
+        // what to print doesn't depend on the reference
+        // But what all methods can we print will depend on the reference type
+        // Here in line number 5 we can see that the object type is Car in car we already are implementing
+        // brake but we cannot access here because the reference type here is Engine in engine we dont anything called as break
+        // Hence it gives an error when we try to print car.brake;
         car.accelarate();
-        car.brake();
+        car.stop();
         car.start();
     }
 }
