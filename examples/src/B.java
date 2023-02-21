7dbcc7fdc15c54edd4f5d98b65effe807f787c97
siddartha-10 <107453857+siddartha-10@users.noreply.github.com>
2023-02-21 11:50:08 -0600

    commit

--- examples/src/B.java
+++ examples/src/B.java
@@ -1,5 +1,24 @@
-public class B {
-    public void greet(){
-        System.out.println("Hi this is class B");
+import java.util.*;
+class B {
+    public static void main(String[] args) {
+        exam ex = new exam1();
+        System.out.println(ex.b);
+        ex.show();
+    }
+}
+class exam{
+    int a = 10;
+    int b = 20;
+
+    void show(){
+        System.out.println("hello 1");
+    }
+}
+class exam1 extends exam{
+    int a = 20000;
+    int b = 210;
+    int c = 500;
+    void show(){
+        System.out.println("hello 2");
     }
 }
