1bcf0ced4b89d30d737457fcdab1b0738edb9ea2
siddartha-10 <107453857+siddartha-10@users.noreply.github.com>
2023-02-23 17:50:32 -0600

    commit

--- Oops/src/Cloning/Human.java
+++ Oops/src/Cloning/Human.java
@@ -3,18 +3,30 @@ package Cloning;
 public class Human implements Cloneable{
     int age;
     String name;
-
+    int[] arr;
     public Human(int age, String name) {
         this.age = age;
         this.name = name;
+        this.arr = new int[]{3,4,5,6,7};
     }
 
-    public Human(Human other) {
-        this.age = other.age;
-        this.name = other.name;
-    }
+    // even without the below code till 16 we can clone the object using objectname.clone().
+//    public Human(Human other) {
+//        this.age = other.age;
+//        this.name = other.name;
+//    }
+
+    // this is called as showllow copy
+//    public Object clone() throws CloneNotSupportedException{
+//        return super.clone();
+//    }
 
     public Object clone() throws CloneNotSupportedException{
-        return super.clone();
+        Human twin = (Human)super.clone();
+        twin.arr = new int[twin.arr.length]; // this is called as deep copy
+        for (int i = 0; i < twin.arr.length; i++) {
+            twin.arr[i] = this.arr[i];
+        }
+        return twin;
     }
 }
--- Oops/src/Cloning/Main.java
+++ Oops/src/Cloning/Main.java
@@ -1,5 +1,7 @@
 package Cloning;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) throws CloneNotSupportedException{
         Human messi = new Human(10,"Lionel messi");
@@ -8,8 +10,21 @@ public class Main {
         * */
         //Human twin_messi = new Human(messi);
 
-        Human twim = (Human)messi.clone();
-        System.out.println(twim.age + " "+twim.name);
+        Human twin = (Human)messi.clone();
+        System.out.println(twin.age + " "+twin.name);
         // what clone does is copy the values of one object to another
+        System.out.println(Arrays.toString(twin.arr));
+        // lets say we change the first element of the cloned object to 100
+        // and check if it's actually changing the original object as well
+        twin.arr[0] = 100;
+        twin.name = "String";
+        twin.age =20;
+        System.out.println(twin.age + " "+twin.name);
+        System.out.println(messi.age + " "+messi.name);
+        System.out.println("This is the array of the twin object"+" "+Arrays.toString(twin.arr));
+        System.out.println("This is the array of the messi object"+" "+Arrays.toString(messi.arr));
+
+        // since changing the cloned object effects or changes the elements in a real object this is called as
+        // shallowed copying
     }
 }
