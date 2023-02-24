d978b74645596bcddf6cb7572fd1b71bc623bc4d
siddartha-10 <107453857+siddartha-10@users.noreply.github.com>
2023-02-23 18:29:03 -0600

    commit

--- Oops/src/Collections/Main.java
+++ Oops/src/Collections/Main.java
@@ -1,9 +1,20 @@
 package Collections;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Collections
+        List<Integer> list1= new ArrayList<>();
+        List<Integer> list2 = new LinkedList<>();
+
+        list2.add(1);
+        list2.add(2);
+        list2.add(3);
+        list2.add(4);
+
+        System.out.println(list2);
     }
 }
