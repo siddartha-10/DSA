ce8bd8f4cd0c4464ea50434c7e814ebab9d3f9e1
siddartha-10 <107453857+siddartha-10@users.noreply.github.com>
2023-02-25 11:00:09 -0600

    Update Basic.java

--- Oops/src/EnumExamples/Basic.java
+++ Oops/src/EnumExamples/Basic.java
@@ -1,5 +1,10 @@
 package EnumExamples;
 
 public class Basic {
-
+ enum week{
+     Monday, Tuesday, Wednesday, Thursday, Friday, Saturday
+     // these are enum constants
+     // the varibales inside an enum are always public static and final
+     // the type of the variables is week.
+ }
 }
