8524fd0963a7aa98190fb0e75cbb4b9bcaa526db
siddartha-10 <107453857+siddartha-10@users.noreply.github.com>
2023-02-25 11:07:12 -0600

    enum commit

--- Oops/src/EnumExamples/Basic.java
+++ Oops/src/EnumExamples/Basic.java
@@ -1,10 +1,20 @@
 package EnumExamples;
 
 public class Basic {
- enum week{
-     Monday, Tuesday, Wednesday, Thursday, Friday, Saturday
+    enum Week{
+     Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
      // these are enum constants
      // the varibales inside an enum are always public static and final
      // the type of the variables is week.
- }
+    }
+
+    public static void main(String[] args) {
+        Week week;
+        week = Week.Sunday;
+        for (Week day:Week.values()) {
+            // here Week.values() will give all the values that are in the Week enumeration.
+            System.out.println(day);
+        }
+        System.out.println(week.ordinal()); // this prints the index of the enum variable the index starts from 0
+    }
 }
