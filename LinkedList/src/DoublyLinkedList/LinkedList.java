99cbd43c9975f28d4f579bbfdb598cf6c1451a25
siddartha-10 <107453857+siddartha-10@users.noreply.github.com>
2023-02-28 10:33:06 -0600

    commit
    Created doubly linkedlist and also created the insertfirst method in that

---
+++ LinkedList/src/DoublyLinkedList/LinkedList.java
@@ -0,0 +1,42 @@
+package DoublyLinkedList;
+
+public class LinkedList {
+
+    Node head;
+
+    // this method is used to insert an element at the first of the linkedlist
+    public void insertFirst(int val){
+        Node node = new Node(val);
+        node.next = head;
+        node.prev = null;
+        if(head != null){
+            head.prev = node;
+        }
+        head = node;
+    }
+
+    // this method is used to display the elements of an DoublyLinkedList
+    public void display(){
+        Node node = head;
+        while(node!=null){
+            System.out.print(node.value + " -> ");
+            node = node.next;
+        }
+        System.out.println("End");
+    }
+    private class Node{
+         int value;
+         Node next;
+         Node prev;
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+        public Node(int value, Node next, Node prev) {
+            this.value = value;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+}
---
+++ LinkedList/src/DoublyLinkedList/Main.java
@@ -0,0 +1,10 @@
+package DoublyLinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList dll = new LinkedList();
+        dll.insertFirst(10);
+        dll.insertFirst(12);
+        dll.display();
+    }
+}
