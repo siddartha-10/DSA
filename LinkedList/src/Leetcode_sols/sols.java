982dca4a4536dc79805b2d52162b54c496ce8300
siddartha-10 <107453857+siddartha-10@users.noreply.github.com>
2023-03-03 18:31:28 -0600

    Update sols.java

--- LinkedList/src/Leetcode_sols/sols.java
+++ LinkedList/src/Leetcode_sols/sols.java
@@ -43,5 +43,21 @@ class Solution {
 * https://leetcode.com/problems/linked-list-cycle/
 *
 * LinkedListCycle
-* 
+* public class solution{
+    public boolean hasCycle(Node head){
+        Node fast = head;
+        Node slow = head;
+
+        while(fast!=null && fast.next!=null){
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if(fast==slow){
+                return true;
+            }
+        }
+        return false;
+    }
+}
+*
 * */
