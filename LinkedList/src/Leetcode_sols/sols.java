8ef1121aeb0d69877b8a3c0682e2bfe7f41f4c6b
siddartha-10 <107453857+siddartha-10@users.noreply.github.com>
2023-03-03 21:00:13 -0600

    Update sols.java

--- LinkedList/src/Leetcode_sols/sols.java
+++ LinkedList/src/Leetcode_sols/sols.java
@@ -162,6 +162,20 @@ public class sols {
         return ans;
     }
 
+    // this is the code to find the middle element of a linkedList in a single pass.
+    // https://leetcode.com/problems/middle-of-the-linked-list/description/
+    public ListNode middleNode(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+
 }
 
 class ListNode {
