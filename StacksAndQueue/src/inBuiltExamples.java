3287359098efbe894d172a0e9fd0579c4b0068e2
siddartha-10 <107453857+siddartha-10@users.noreply.github.com>
2023-03-09 17:49:07 -0600

    stack commit
    Using inbuilt stack
    Did 2 operations 1) push 2) pop

--- StacksAndQueue/src/inBuiltExamples.java
+++ StacksAndQueue/src/inBuiltExamples.java
@@ -1,5 +1,19 @@
+import java.util.Stack;
+
 public class inBuiltExamples {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Stack<Integer> stack = new Stack<>();
+        stack.push(34);
+        stack.push(45);
+        stack.push(2);
+        stack.push(9);
+        stack.push(18);
+
+        System.out.println(stack.pop()); // this will print 18
+        System.out.println(stack.pop()); // this will print 9
+        System.out.println(stack.pop()); // this will print 2
+        System.out.println(stack.pop()); // this will print 45
+        System.out.println(stack.pop()); // this will print 34
+
     }
 }
