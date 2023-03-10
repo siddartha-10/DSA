8be44550c6b1873d0e817f1791b35ffdc56d139a
siddartha-10 <107453857+siddartha-10@users.noreply.github.com>
2023-03-09 18:09:56 -0600

    queue commit
    Queue method
    1) add :-  used to add behind.
    2) peek() :- used to just see the first element.
    3) remove() :- used to remove the element.

--- StacksAndQueue/src/inBuiltExamples.java
+++ StacksAndQueue/src/inBuiltExamples.java
@@ -1,7 +1,10 @@
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 public class inBuiltExamples {
     public static void main(String[] args) {
+        /*
         Stack<Integer> stack = new Stack<>();
         stack.push(34);
         stack.push(45);
@@ -14,6 +17,20 @@ public class inBuiltExamples {
         System.out.println(stack.pop()); // this will print 2
         System.out.println(stack.pop()); // this will print 45
         System.out.println(stack.pop()); // this will print 34
+         */
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(3);
+        queue.add(6);
+        queue.add(5);
+        queue.add(19);
+        queue.add(1);
 
+        /*
+        System.out.println(queue.peek());
+        this is just used to see the first element of the queue
+        it will not remove the element.
+         */
+
+        System.out.println(queue.remove()); // this will completely remove the element from the queue.
     }
 }
