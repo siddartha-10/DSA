7599754797451b2bb262a9eee01fc3af703c78e9
siddartha-10 <107453857+siddartha-10@users.noreply.github.com>
2023-03-10 09:48:55 -0600

    Update CustomStack.java

--- StacksAndQueue/src/CustomStack.java
+++ StacksAndQueue/src/CustomStack.java
@@ -1,5 +1,16 @@
 public class CustomStack {
-    public static void main(String[] args) {
+    protected int[] data;
+    private static final int Default_Size = 10;
 
+    /*
+    Here we are using this keyword to call the constructor with a parameter as size.
+    Whenever we want to call the default constructor we will use the default size.
+     */
+    public CustomStack() {
+        this(Default_Size);
+    }
+
+    public CustomStack(int size) {
+        this.data = data;
     }
 }
