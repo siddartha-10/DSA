a599d8b4201c3cedda4368e541b741e5fac81eb0
siddartha-10 <107453857+siddartha-10@users.noreply.github.com>
2023-03-12 11:44:20 -0500

    commit

---
+++ examples/src/example.java
@@ -0,0 +1,16 @@
+import java.sql.SQLOutput;
+
+public class example {
+    public static void main(String[] args) {
+        int cnt = 400;
+        while(cnt>=1){
+            cnt--;
+            System.out.println(cnt);
+
+        }
+//        int cnt = 2;
+//        while(cnt < 5){
+//            cnt = cnt + 1;
+//            System.out.println("cnt = "+ cnt);
+    }
+}
