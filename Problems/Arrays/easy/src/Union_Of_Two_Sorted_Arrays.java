4bdf0aa388c102f3d924f57204489bc2005e176b
siddartha-10 <107453857+siddartha-10@users.noreply.github.com>
2023-03-16 13:40:27 -0500

    commit

--- Problems/Arrays/easy/src/Union_Of_Two_Sorted_Arrays.java
+++ Problems/Arrays/easy/src/Union_Of_Two_Sorted_Arrays.java
@@ -1,15 +1,48 @@
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
+import java.util.*;
 
 // https://practice.geeksforgeeks.org/problems/union-of-two-sorted-arrays-1587115621/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=union-of-two-sorted-arrays
 public class Union_Of_Two_Sorted_Arrays {
     public static void main(String[] args) {
         int[] arr1 = {1, 2, 3, 4, 5};
         int[] arr2 = {1, 2, 3,6};
-        brute_force(arr1,arr2);
+        //brute_force(arr1,arr2);
+        System.out.println(optimal_solution(arr1,arr2));
 
+    }
+
+    private static ArrayList<Integer> optimal_solution(int[] arr1, int[] arr2) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        int i = 0; // this is the pointer for the first array
+        int j = 0; // this is the pointer for the second array
+        while(i<arr1.length && j<arr2.length){
+            if(arr1[i]<=arr2[j]){
+                if(arr.isEmpty() || arr.get(arr.size()-1)!=arr1[i]){
+                    arr.add(arr1[i]);
+                }
+                i++;
+            }
+            else{
+                if(arr.isEmpty() || arr.get(arr.size()-1)!=arr2[j]){
+                    arr.add(arr2[j]);
+                }
+                j++;
+            }
+        }
+
+        while(i<arr1.length){
+            if(arr.isEmpty() || arr.get(arr.size()-1)!=arr1[i]){
+                arr.add(arr1[i]);
+            }
+            i++;
+        }
+
+        while(j<arr2.length){
+            if(arr.isEmpty() || arr.get(arr.size()-1)!=arr2[j]){
+                arr.add(arr2[j]);
+            }
+            j++;
+        }
+        return arr;
     }
 
     private static void brute_force(int[] arr1, int[] arr2) {
@@ -24,7 +57,7 @@ public class Union_Of_Two_Sorted_Arrays {
         Iterator<Integer> it = hs.iterator();
         int i = 0;
         while(it.hasNext()){
-            union[i++]
+            union[i++] = it.next();
         }
         System.out.println(Arrays.toString(union));
     }
