a93dd9f5d0a39234e263c119b7c9e394d5f169b9
siddartha-10 <107453857+siddartha-10@users.noreply.github.com>
2023-03-16 11:51:35 -0500

    optimal

--- Problems/Arrays/easy/src/MoveAllZeroesToEndofArray.java
+++ Problems/Arrays/easy/src/MoveAllZeroesToEndofArray.java
@@ -9,14 +9,16 @@ public class MoveAllZeroesToEndofArray {
 
     private static void optimal_solution(int[] arr) {
         // step1 : to find the first occurance of zero
-        int j = 0;
-        while(true){
-            if (arr[j]!=0){
-                j++;
-            }
-            else{
+        int j = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]==0){
+                j = i;
                 break;
             }
+        }
+        // if there are no Zero's in the array then you can just return the same array
+        if(j==-1){
+            return;
         }
         // step2 : start the iteration from the next element of the zero
         for (int i = j+1; i < arr.length; i++) {
