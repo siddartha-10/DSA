e8fb26a4728c49df2a7f577be98b9a71be8f4180
siddartha-10 <107453857+siddartha-10@users.noreply.github.com>
2023-03-18 14:17:47 -0500

    commit

--- Problems/easy/src/Intersection_of_two_arrays.java
+++ Problems/easy/src/Intersection_of_two_arrays.java
@@ -7,7 +7,28 @@ public class Intersection_of_two_arrays {
     public static void main(String[] args) {
         int[] arr1 = {1,2,2,3,3,4,5,6};
         int[] arr2 = {2,3,3,5,6,6,7};
-        brute_force(arr1,arr2);
+        //brute_force(arr1,arr2);
+        optimal_sol(arr1,arr2);
+    }
+
+    private static void optimal_sol(int[] arr1, int[] arr2) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        int i=0;
+        int j = 0;
+        while(i<arr1.length && j<arr2.length){
+            if(arr1[i]==arr2[j]){
+                arr.add(arr1[i]);
+                i++;
+                j++;
+            }
+            else if(arr1[i]<arr2[j]){
+                i++;
+            }
+            else{
+                j++;
+            }
+        }
+        System.out.println(arr);
     }
 
     private static void brute_force(int[] arr1, int[] arr2) {
