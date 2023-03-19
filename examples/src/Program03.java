a7044b48c6a474cc43d97e7c34d7da37e4a9fa45
siddartha-10 <107453857+siddartha-10@users.noreply.github.com>
2023-03-19 11:56:25 -0500

    commit

--- examples/src/Program03.java
+++ examples/src/Program03.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
 public class Program03 {
     public static void main(String[] args) {
-        for (int i = 0; i < 14; i++) {
-            System.out.println(i);
+        for (int i = 2; i <=40; i++) {
+            if(i%2==0){
+                System.out.println(i);
+            }
         }
     }
 }
