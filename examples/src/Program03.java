bdbd1438d86db61b58c4057d228b53f733a7be51
siddartha-10 <107453857+siddartha-10@users.noreply.github.com>
2023-03-19 11:31:06 -0500

    Update Program03.java

--- examples/src/Program03.java
+++ examples/src/Program03.java
@@ -1,29 +1,8 @@
 import java.util.Scanner;
 public class Program03 {
     public static void main(String[] args) {
-        Scanner stdIn = new Scanner(System.in);
-
-        //      Input
-        System.out.print("Enter the Pitcher's First Name: ");   //  Inputting FirstName
-        String FirstName = stdIn.next();
-        System.out.print("Enter the Pitcher's Last Name: ");    //  Inputting Last Name
-        String LastName = stdIn.next();
-        int runs;         //  Declaring variables & inputting numbers
-        int innings;
-        System.out.print("Number of earned runs: ");
-        runs = stdIn.nextInt();
-        System.out.print("Number of innings pitched: ");
-        innings = stdIn.nextInt();
-        Double r = Double.valueOf(runs);        //  Converting int to double
-        Double i = Double.valueOf(innings);
-        double ERA;
-        ERA = r*9 / i;                          //Formula to get ERA
-//      Output
-        System.out.println('\n' + "Pitcher's First Name: " + FirstName);
-        System.out.print("Pitcher's Last Name: " + LastName);
-        System.out.print('\n' + "Number of earned runs: " + runs);
-        System.out.print('\n' + "Number of innings pitched: " + innings);
-        System.out.print('\n' + FirstName + '\t' + LastName + " has an ERA of " + ERA);
-        stdIn.close();
+        for (int i = 0; i < 14; i++) {
+            System.out.println(i);
+        }
     }
 }
