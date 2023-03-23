ed10f3ab00329eacfa7c9766ed60b87bec87616b
siddartha-10 <107453857+siddartha-10@users.noreply.github.com>
2023-03-22 20:02:33 -0500

    Update Program03.java

--- examples/src/Program03.java
+++ examples/src/Program03.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Program03 {
     public static void main(String[] args) {
         int cnt = 0;
-        while(cnt >=2 && cnt<=40){
+        while(cnt >=2 && cnt<=40)
         }
     }
 }
